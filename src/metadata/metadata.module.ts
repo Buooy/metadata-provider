976c9a6d515afd1de8962ac5254c98424daef47a
Buooy <aaron.lee@buooy.com>
2022-09-23 10:41:19 +0800

    chore(ci): deploying s3 and serverless cloud

--- src/metadata/metadata.module.ts
+++ src/metadata/metadata.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { S3Module } from 'nestjs-s3';
 import { MetadataService } from './metadata.service';
 import { MetadataController } from './metadata.controller';
 
 @Module({
+  imports: [
+    S3Module.forRootAsync({
+      useFactory: () => ({
+        config: {
+          accessKeyId: 'minio',
+          secretAccessKey: 'password',
+          endpoint: 'http://localhost:9000',
+          s3ForcePathStyle: true,
+          signatureVersion: 'v4',
+        },
+      }),
+    }),
+  ],
   controllers: [MetadataController],
   providers: [MetadataService]
 })
