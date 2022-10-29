42b30290d3573b316a0e56fa7b7e4e4c2edbc67e
Buooy <aaron.lee@buooy.com>
2022-10-29 15:11:02 +0800

    feat(metadata): added feature to generate json files to s3 bucket

--- src/metadata/metadata.controller.ts
+++ src/metadata/metadata.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { MetadataService } from './metadata.service';
 import { NotFoundException } from '@nestjs/common';
 
@@ -7,15 +15,33 @@ export class MetadataController {
   constructor(private readonly metadataService: MetadataService) {}
 
   @Get(':subject')
-  async findAll(@Param('subject') subject: string): Promise<Record<any, any>[]> {
+  async findAll(
+    @Param('subject') subject: string,
+  ): Promise<Record<any, any>[]> {
     const records = await this.metadataService.findAll(subject);
 
     if (records === undefined) throw new NotFoundException();
     return records;
   }
 
+  @Get(':subject/generate-json')
+  async generateJsonFiles(
+    @Param('subject') subject: string,
+  ): Promise<Record<any, any>[]> {
+    const records = await this.metadataService.findAll(subject);
+
+    if (records === undefined) throw new NotFoundException();
+
+    await this.metadataService.storeInObjectStorage(subject, records);
+
+    return records;
+  }
+
   @Get(':subject/:id')
-  async findOne(@Param('subject') subject: string, @Param('id') id: string): Promise<Record<any, any>> {
+  async findOne(
+    @Param('subject') subject: string,
+    @Param('id') id: string,
+  ): Promise<Record<any, any>> {
     const record = await this.metadataService.findOne(subject, id);
 
     if (record === undefined) throw new NotFoundException();
--- src/metadata/metadata.service.ts
+++ src/metadata/metadata.service.ts
@@ -1,40 +1,72 @@
-import {dirname} from 'path';
+import { dirname } from 'path';
 import { Injectable } from '@nestjs/common';
 import csv from 'csvtojson';
 import { S3 } from 'aws-sdk';
 import { InjectS3 } from 'nestjs-s3';
 
+type MetadataRecord = Record<any, any>;
+
 @Injectable()
 export class MetadataService {
-  constructor(
-    @InjectS3() private readonly s3: S3,
-    ) {}
+  constructor(@InjectS3() private readonly s3: S3) {}
 
-  async parseMetadata(subject: string): Promise<Record<any, any>[]> {
+  async parseMetadata(subject: string): Promise<MetadataRecord[]> {
     try {
-      const readStream = this.s3.getObject({
-        Bucket: String(process.env.S3_BUCKET),
-        Key: `${subject}.csv`,
-      }).createReadStream()
+      const readStream = this.s3
+        .getObject({
+          Bucket: String(process.env.S3_BUCKET),
+          Key: `${subject}.csv`,
+        })
+        .createReadStream();
 
       const records = await csv().fromStream(readStream);
 
       return records;
     } catch (error) {
-      console.error(error)
+      console.error(error);
       return [];
     }
   }
-  
-  async findAll(subject: string): Promise<Record<any, any>[]|undefined> {
-    const records = await this.parseMetadata(subject);
-    return records
-  }
-  
-  async findOne(subject: string, id: number|string): Promise<Record<any, any>|undefined> {
-    const records = await this.parseMetadata(subject);
-    const record = records.find(row => row.id === id);
 
-    return record
+  async findAll(subject: string): Promise<Record<any, any>[] | undefined> {
+    const records = await this.parseMetadata(subject);
+    return records;
+  }
+
+  async findOne(
+    subject: string,
+    id: number | string,
+  ): Promise<Record<any, any> | undefined> {
+    const records = await this.parseMetadata(subject);
+    const record = records.find((row) => row.id === id);
+
+    return record;
+  }
+
+  async storeInObjectStorage(
+    subject: string,
+    records: MetadataRecord[],
+    recordPrefix = '',
+    recordSuffix = '',
+  ) {
+    for (const record of records) {
+      const buf = Buffer.from(JSON.stringify(record));
+
+      const data = {
+        Bucket: String(process.env.S3_JSON_BUCKET),
+        Key: `${subject}/${recordPrefix}${record.id}${recordSuffix}.json`,
+        Body: buf,
+        ContentEncoding: 'base64',
+        ContentType: 'application/json',
+      };
+
+      try {
+        const result = await this.s3.upload(data).promise();
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+        console.log('Error uploading data: ', data);
+      }
+    }
   }
 }
